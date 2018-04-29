88ce7bde6d87170dc61ec3d5928e4e01de80fb59
tradivarius <dmr_2910@hotmail.es>
2018-04-29 11:57:20 +0200

    test

---
+++ eshop4/test/integration/browsing_and_searching_test.rb
@@ -0,0 +1,90 @@
+require 'test_helper'
+
+class BrowsingAndSearchingTest < ActionDispatch::IntegrationTest
+  fixtures :suppliers, :producers, :liquors, :liquors_suppliers
+
+  test "browse" do
+    jill = new_session_as :jill
+    jill.index
+    jill.second_page
+    jill.liquor_details 'Pride and Prejudice'
+    jill.latest_liquors
+  end
+
+  module BrowsingTestDSL
+    include ERB::Util
+    attr_writer :name
+
+    def index
+      get '/catalog/index'
+      assert_response :success
+      assert_select 'dl#liquors' do
+        assert_select 'dt', :count => 5
+      end
+      # assert_tag :tag => 'dl', :attributes => { :id => 'books' },
+      #            :children => { :count => 5, :only => { :tag => 'dt' }}
+      assert_select 'dt' do
+        assert_select 'a', 'The Idiot'
+      end
+      # assert_tag :tag => 'dt', :content => 'The Idiot'
+      check_liquor_links
+    end
+
+    def second_page
+      get '/catalog/index?page=2'
+      assert_response :success
+      assert_template 'catalog/index'
+      assert_equal Liquor.find_by_title('Pro Rails E-Commerce'),
+                   assigns(:liquors).last
+      check_liquor_links
+    end
+
+    def liquor_details(title)
+      @liquor = Liquor.where(:name => title).first
+      get "/catalog/show/#{@liquor.id}"
+      assert_response :success
+      assert_template 'catalog/show'
+      assert_select 'div#content' do
+        assert_select 'h1', @liquor.name
+        assert_select 'h2', "by #{@liquor.suppliers.map{|a| a.name}.join(", ")}"
+      end
+      # assert_tag :tag => 'h1', :content => @book.title
+      # assert_tag :tag => 'h2', :content => "by #{@book.authors.map{|a| a.name}.join(", ")}"
+    end
+
+    def latest_liquors
+      get '/catalog/latest'
+      assert_response :success
+      assert_template 'catalog/latest'
+      assert_select 'dl#liquors' do
+        assert_select 'dt', :count => 5
+      end
+      # assert_tag :tag => 'dl', :attributes => { :id => 'books' },
+      #            :children => { :count => 5, :only => { :tag => 'dt' } }
+      @liquors = Liquor.latest(5)
+      @liquors.each do |a|
+        assert_select 'dt' do
+          assert_select 'a', a.title
+        end
+        # assert_tag :tag => 'dt', :content => a.title
+      end
+    end
+
+    def check_liquor_links
+      for liquor in assigns :liquors
+        assert_select 'a' do
+          assert_select '[href=?]', "/catalog/show/#{liquor.id}"
+        end
+        # assert_tag :tag => 'a', :attributes => { :href => "/catalog/show/#{book.id}" }
+      end
+    end
+  end
+
+  def new_session_as(name)
+    open_session do |session|
+      session.extend(BrowsingTestDSL)
+      session.name = name
+      yield session if block_given?
+    end
+  end
+end
---
+++ eshop4/test/integration/liquour_administration_test.rb
@@ -0,0 +1,107 @@
+require 'test_helper'
+
+class LiquourAdministrationTest < ActionDispatch::IntegrationTest
+
+  test "liquour_aministration" do
+    producer = Producer.create(:name => 'Books of Ruby')
+    supplier = Supplier.create(:first_name => 'John', :last_name => 'Anderson')
+    george = new_session_as(:george)
+
+    new_liquour_ruby = george.add_liquour:liquour=> {
+      :name => 'A new Liquour of Ruby',
+      :producer_id => producer.id,
+      :supplier_ids => [supplier.id],
+      :produced_at => Time.now,
+      :serial_number => '123',
+      :blurb => 'A new Liquour of Ruby',
+      :price => 45.5
+    }
+
+    george.list_liquours
+    george.show_liquour new_liquour_ruby
+
+    george.edit_liquour new_liquour_ruby, :liquour=> {
+      :name => 'A very new Liquour of Ruby',
+      :producer_id => producer.id,
+      :supplier_ids => [supplier.id],
+      :produced_at => Time.now,
+      :serial_numbe => '123',
+      :blurb => 'A very new Liquour of Ruby',
+      :price => 50
+    }
+
+    bob = new_session_as(:bob)
+    bob.delete_liquour new_book_ruby
+  end
+
+  private
+
+  module LiquourTestDSL
+    attr_writer :name
+
+    def add_liquour(parameters)
+      supplier = Supplier.first
+      producer = Producer.first
+      get '/admin/liquour/new'
+      assert_response :success
+      assert_template 'admin/liquour/new'
+      assert_select 'select#liquour_producer_id' do
+        assert_select "option[value=\"#{producer.id}\"]", producer.name
+      end
+      # assert_tag :tag => 'option', :attributes => { :value => publisher.id }
+      assert_select "select[name=\"liquour[supplier_ids][]\"]" do
+        assert_select "option[value=\"#{supplier.id}\"]", supplier.name
+      end
+      # assert_tag :tag => 'select', :attributes => { :name => 'book[author_ids][]' }
+      post '/admin/liquour/create', parameters
+      assert_response :redirect
+      follow_redirect!
+      assert_response :success
+      assert_template 'admin/liquour/index'
+      page = Liquour.all.count / 5 + 1
+      get "/admin/liquour/index/?page=#{page}"
+      assert_select 'td', parameters[:book][:title]
+      # assert_tag :tag => 'td', :content => parameters[:book][:title]
+      liquour = Liquour.find_by_title(parameters[:liquour][:title])
+      return liquour;
+    end
+
+    def edit_liquour(liquour, parameters)
+      get "/admin/liquour/edit?id=#{liquour.id}"
+      assert_response :success
+      assert_template 'admin/liquour/edit'
+      post "/admin/liquour/update?id=#{liquour.id}", parameters
+      assert_response :redirect
+      follow_redirect!
+      assert_response :success
+      assert_template 'admin/liquour/show'
+    end
+
+    def delete_liquour(liquour)
+      post "/admin/liquour/destroy?id=#{liquour.id}"
+      assert_response :redirect
+      follow_redirect!
+      assert_template 'admin/liquour/index'
+    end
+
+    def show_liquour(liquour)
+      get "/admin/liquour/show/#{liquour.id}"
+      assert_response :success
+      assert_template 'admin/liquour/show'
+    end
+
+    def list_liquours
+      get '/admin/liquour/index'
+      assert_response :success
+      assert_template 'admin/liquour/index'
+    end
+  end
+
+  def new_session_as(name)
+    open_session do |session|
+      session.extend(LiquourTestDSL)
+      session.name = name
+      yield session if block_given?
+    end
+  end
+end
