f34ca6fcfa9324366bd2259d0f42de452e7944ff
FrkWTF <FrkWTF@gmail.com>
2018-05-07 12:43:53 +0200

    Modelos order

--- eshop4/app/models/order_item.rb
+++ eshop4/app/models/order_item.rb
@@ -1,2 +1,9 @@
 class OrderItem < ActiveRecord::Base
+  belongs_to :order
+  belongs_to :liquor
+
+  def validate
+    errors.add(:amount, "debe ser uno o mas") unless amount.nil? || amount > 0
+    errors.add(:price, "debe ser un número positivo") unless price.nil? || price > 0.0
+  end
 end
